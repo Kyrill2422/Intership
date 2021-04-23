4e836bab3a05b14e18bc8b4714ebd6021f536f23
Catrina Dan <catrinailiedan19@stud.ase.ro>
2021-04-23 18:33:30 +0300

    v0.001

---
+++ uproject/src/main/java/com/project/catalog/web/dto/SubjectDto.java
@@ -0,0 +1,10 @@
+package com.project.catalog.web.dto;
+
+public class SubjectDto {
+
+    private String name;
+    private Integer creditPuncte;
+    private boolean isOptional;
+    
+
+}
---
+++ uproject/src/main/java/com/project/catalog/web/dto/TeacherDto.java
@@ -0,0 +1,11 @@
+package com.project.catalog.web.dto;
+
+import java.time.LocalDate;
+
+public class TeacherDto {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private LocalDate employmentDate;
+}
